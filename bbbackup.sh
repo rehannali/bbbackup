5e0f3a585c4f8db6f58aa8808abc6bf5e1dd2ecf
Rehan Ali <rehannali@programmer.net>
2024-05-13 19:14:59 +0500

    modify automation script so no one needs to update path
    added capture logs

--- bbbackup.sh
+++ bbbackup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# activate virtual environment for bitbucket backup script
-cd /Users/admin/Backup/
-source bin/activate
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+exec >> "$SCRIPT_DIR"/output.log 2>&1
+# shellcheck disable=SC2164
+cd "$SCRIPT_DIR"
 
 # execute backup script to do a backup to the defined folder (see bbbackup.cfg which folder and which config the app will run with)
-python bbbackup.py --configuration /Users/admin/Backup/bbbackup.cfg --notify --backup
+poetry run python bbbackup.py --configuration "$SCRIPT_DIR"/bbbackup.cfg --backup
